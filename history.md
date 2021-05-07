1aa5d12a551f394b6019f887ec31f66250064044
liudaojie <liudaojie@yy.com>
2021-05-07 19:33:28 +0800

    feat: 更新 `yyl-base-webpack-config@0.2.0`

--- history.md
+++ history.md
@@ -1,5 +1,9 @@
 # 版本变更
 
+## 0.2.0 (2021-05-07)
+
+- feat: 更新 `yyl-base-webpack-config@0.2.0`
+
 ## 0.1.1 (2021-04-06)
 
 - feat: 兼容 yyl-base-webpack-config 配置
--- output/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Configuration } from 'webpack';
-import { YylBaseInitConfigOption } from 'yyl-base-webpack-config';
-export declare type WConfigVue2Result = Required<Pick<Configuration, 'module' | 'resolve' | 'plugins'>>;
-declare function yylVue2WebpackConfig(op: YylBaseInitConfigOption): Configuration;
-export default yylVue2WebpackConfig;
